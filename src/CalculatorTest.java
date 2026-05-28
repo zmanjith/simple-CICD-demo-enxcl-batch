d2250bb1e82a7f419b12c5cef7d98584f33043ed
zmanjith <zmanjith@gmail.com>
2026-05-28 00:26:57 -0500

    CI CD and Testing

---
+++ src/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+
+        assertEquals(5, calc.add(2,3));
+
+    }
+}
